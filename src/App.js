97ebc18fca923f96d83caf151a1f9fb930879269
Swaroopkumar07 <painamswaroopkumar@gmail.com>
2024-05-04 19:25:39 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import InputModal from '../src/Components/InputModal/InputModal'
+
+export default function App() {
+  return (
+    <div>
+      <InputModal/>
+    </div>
+  )
+}
